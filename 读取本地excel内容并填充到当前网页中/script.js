0d4419d2bff901e3d59d7e162696ddec7cc114b2
jeasonchan <439994526@qq.com>
2019-11-18 23:53:27 +0800

    init script file

---
+++ 读取本地excel内容并填充到当前网页中/script.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tampermonkey.net/faq.php?version=4.9.6095&ext=fire
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+
+
+
+
+
+
+
+
+    
+})();
