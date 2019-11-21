73baa456bbfffa933e0ea83bcc8e46311951e645
jeasonchan <439994526@qq.com>
2019-11-22 00:53:59 +0800

    add typescript to parse xlsx file

---
+++ Main.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var Xlsx = require("xlsx");
+console.log("Start running......");
+var excelFile = Xlsx.readFile("../tampermonkey-scriptps/resource/新建 Microsoft Excel 工作表.xlsx");
+var sheetList = excelFile.SheetNames;
+console.log("sheet有：" + sheetList);
+console.log(excelFile.Props);
+console.log(excelFile.Sheets);
+console.log("start print sheet in cycle:");
+for (var i = 0; i < sheetList.length; i++) {
+    console.log(sheetList[i]);
+    var sheetObject = excelFile.Sheets[sheetList[i]];
+    console.log(sheetObject);
+    // console.log(Xlsx.utils.sheet_to_json(sheetObject));
+}
---
+++ Main.ts
@@ -0,0 +1,21 @@
+import * as Xlsx from 'xlsx'
+
+console.log("Start running......")
+
+
+let excelFile: Xlsx.WorkBook = Xlsx.readFile("../tampermonkey-scriptps/resource/新建 Microsoft Excel 工作表.xlsx");
+let sheetList: string[] = excelFile.SheetNames;
+console.log("sheet有：" + sheetList);
+console.log(excelFile.Props);
+console.log(excelFile.Sheets)
+
+console.log("start print sheet in cycle:")
+
+for (let i: number = 0; i < sheetList.length; i++) {
+    console.log(sheetList[i]);
+    let sheetObject: Xlsx.WorkSheet = excelFile.Sheets[sheetList[i]];
+    console.log(sheetObject);
+    // console.log(Xlsx.utils.sheet_to_json(sheetObject));
+}
+
+// https://www.jianshu.com/p/7d2e584cbcc1
