34bc3e5d8fa3a283bf400651343b6452da53b259
jeasonchan <439994526@qq.com>
2019-12-01 23:04:31 +0800

    新增未解之谜.ts;增加接口规定函数的输入输出未解知识点;增加function定义类未解点

---
+++ typescript_未解之谜.ts
@@ -0,0 +1,48 @@
+//function  关键字定义类
+function Person(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.type = "Person";
+new Person("jeason", 12);
+
+
+
+//===========================ts中接口独有的用法？？？？？？
+//使用接口约束函数的入参和返回类型？？？？？？？
+interface FetchInstance {
+    (config: {}): {};
+    (url: string, config: {}): {};
+}
+
+
+// 定义具有两个重载的 fetch0
+function fetch0(config: {}): {};
+function fetch0(url: string, config: {}): {};
+
+function fetch0(url: string | {}, config?: {}): {} {
+    return {};
+}
+
+// 赋值给 FetchInstance 类型的变量无压力
+const fetch1: FetchInstance = fetch0;
+
+var test_var:FetchInstance={};//接口规定了函数的输入和方法两个方面；
+//其实应该赋值一个函数对象，输入和输出都和接口里定义的相同才行
+
+//类型和FetchInstance不符合
+class hahaha implements FetchInstance {
+}
+
+
+//===========================类似于java中的接口用法
+interface Operate {
+    add(number1: number): number;
+}
+
+class Test implements Operate {
+    add(number1: number): number {
+        throw new Error("Method not implemented.");
+    }
+
+}
