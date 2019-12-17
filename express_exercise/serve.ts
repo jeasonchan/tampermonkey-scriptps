0c31a52a27743d303dbf5681c1e5f9850a96212b
jeasonchan <439994526@qq.com>
2019-12-17 21:48:00 +0800

    update

--- express_exercise/serve.ts
+++ express_exercise/serve.ts
@@ -1,21 +1,18 @@
-import * as Express from "express";//单独导入的Exprss命名空间
-const express = require("express");//单独导入express构造函数
+import * as Express_NameSpace from "express"  //这种方法默认导入的是命名空间 e
+var server: Express_NameSpace.Express = {} as Express_NameSpace.Express
 
+import Express = require("express")  //这种方式不能导出明明空间，所以，导出的就只是 function e了
 
+var appjijii: Express_NameSpace.Express = Express()
+appjijii.get("/api", (request, response) => {
+    console.log(`got ${request}`)
+    response.send({ "code": 0, "message": "success" });
+});
 
-let server: Express.Express = express();
-
-server.get('/', handle);//相当于传递了一个对象进来，或者用lambda表达式也可以
-
-server.listen(3000, function () {
+appjijii.listen(3000, function () {
     console.log("server is lietening to port 3000");
 });
 
 
-function handle(req: Request, res: any) {
-    console.log("got request from" + req);
-    res.send('Hello World!')
-}
 
-
-
+//最正确的导入方法！！！！主要是还是因为express模块写的不好，没有默认的到导出，导致ts原生导出无法识别
