4e139bc98dc3f90c8fe4ac0a945c8abf1247f980
jeasonchan <439994526@qq.com>
2019-12-25 23:59:59 +0800

    update

--- 读取本地excel内容并填充到当前网页中/script.js
+++ 读取本地excel内容并填充到当前网页中/script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         读取Excel填充网页
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -15,47 +15,46 @@
 
 
 
+'use strict';
+// Your code here...
 
-(function () {
-    'use strict';
-    // Your code here...
-    
-    //添加控件        
-    $(document).ready(function () {
-        $('body').prepend('<input type="file" id="excel_file">');
-        $('body').prepend('<button type="button" id="open_excel_button">选择Excel</button>');
-        $("#open_excel_button").on("click", parseFile);
-    });
-
-    console.log("haah");
+//添加控件        
+$(document).ready(function () {
+    $('body').prepend('<input type="file" style="display:None" id="excel_file">');
+    $("#open_excel_button").on("click", parseFile);
+    $('body').prepend('<button type="button" id="open_excel_button">选择Excel</button>');
+    $("#open_excel_button").on("click", parseFile);
+});
 
 
 
 
-    //======================funciton define start=======================
-    function printEachProperity(object, properity) {
-        alert(properity);
+//======================funciton define start=======================
+function printEachProperity(object, properity) {
+    alert(properity);
+}
+
+
+function parseFile() {
+    // $("#excel_file").click();  //在ready函数外面不能使用，只能通过原始的文档树进行操作
+    document.getElementById('excel_file').click();
+
+    var files = document.getElementById("excel_file").prop("files");
+    console.log(files)
+    // var files = $("#excel_file").prop('files');//获取到文件列表
+    var reader = new FileReader();//新建一个FileReader
+    reader.onload = function (evt) { //读取完文件之后会回来这里
+        console.log("hhh");
+        var fileString = evt.target.result;
+        console.log(a);
     }
 
-
-    function parseFile() {
-        console.log("click")
-        $("#excel_file").click();
-        console.log("click2")
-        var files = $('input[id="excel_file"]').prop('files');//获取到文件列表
-        var reader = new FileReader();//新建一个FileReader
-        reader.onload = function (evt) { //读取完文件之后会回来这里
-            var fileString = evt.target.result;
-            console.log(a);
-        }
-
-    }
+}
 
 
-    function printAlert() {
-        alert("WORKS");
-    }
+function printAlert() {
+    alert("WORKS");
+}
     //======================funciton define end=======================
 
 
-})();
